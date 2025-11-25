301e7ac523c19d030bc56d9877391f7f7b45026f
barberking1ag-maker <barberking1.ag@gmail.com>
2025-11-25 00:01:42 -0600

    Create App component for digital memorial platform

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Opictuary</h1>
+      <p>Digital Memorial Platform</p>
+    </div>
+  );
+}
+
+export default App;
