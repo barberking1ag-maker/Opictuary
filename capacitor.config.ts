646886087228ae271bde6c481af4e8d946b4fa52
barberking1ag-maker <barberking1.ag@gmail.com>
2025-11-26 10:55:36 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,39 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.opictuary.memorial',
   appName: 'Opictuary',
-  webDir: 'client/dist',
+  webDir: 'dist/public',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https',
+    cleartext: false
+  },
   android: {
     buildOptions: {
       versionCode: 2025112601,
       versionName: '2.1.0'
+    }
+  },
+  ios: {
+    contentInset: 'automatic',
+    limitsNavigationsToAppBoundDomains: true
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#2C1810",
+      showSpinner: false,
+      iosSpinnerStyle: 'small',
+      splashFullScreen: true,
+      splashImmersive: true
+    },
+    StatusBar: {
+      style: 'dark',
+      backgroundColor: "#2C1810"
+    },
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert']
     }
   }
 };
