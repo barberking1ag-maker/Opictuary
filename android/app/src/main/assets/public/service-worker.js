e22024e55440c404042110d3b4c7ec0113461029
barberking1ag-maker <barberking1.ag@gmail.com>
2026-01-29 11:32:06 +0000

    Build update 2026-01-29

--- android/app/src/main/assets/public/service-worker.js
+++ android/app/src/main/assets/public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'opictuary-v2';
-const STATIC_CACHE = 'opictuary-static-v2';
-const DYNAMIC_CACHE = 'opictuary-dynamic-v2';
+const CACHE_NAME = 'opictuary-v1';
+const STATIC_CACHE = 'opictuary-static-v1';
+const DYNAMIC_CACHE = 'opictuary-dynamic-v1';
 
 const STATIC_ASSETS = [
   '/',
